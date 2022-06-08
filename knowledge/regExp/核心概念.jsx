018721d7d0b7b3b09d51be1a6ad76fd3ef826800
shaohua.sun <shaohua.sun@transwarp.cn>
2022-06-08 14:13:15 +0800

    docs: 修改'生命周期文档'

---
+++ knowledge/regExp/核心概念.jsx
@@ -0,0 +1,11 @@
+// 1.hello world
+ReactDOM.render(<div>hello world</div>,document.getElementById('root'));
+
+// 2.react为什么使用jsx
+jsx是javaScript的扩展语法
+
+//3.state和生命周期
+https://react.docschina.org/docs/state-and-lifecycle.html
+
+//4.事件处理
+react的事件命名是小驼峰的形式.reatApp
