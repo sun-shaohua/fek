8a56c7f22f20fffc86d372fbde0d499805f5d39e
shaohua.sun <shaohua.sun@transwarp.cn>
2022-06-23 15:46:53 +0800

    docs: add 泛型

---
+++ knowledge/typeScript/泛型.ts
@@ -0,0 +1,20 @@
+/*泛型工具*/
+/* 把泛型中的所有类型变成可选类型*/
+type _Partial<T> = { [P in keyof T]?: T[P] }
+/* 把K中的所有属性转化成T类型*/
+type _Record<K extends keyof any, T> = { [key in K]: T }
+/*把T中的K提取出来，生成新的键值对类型*/
+type _Pick<T, K extends keyof T> = { [P in K]: T[P] }
+/*在T类型中，去除T类型和U类型的交集*/
+type _Exclude<T, U> = T extends U ? never : T
+/*适用于键值对对象中，去除T中包含K的键值对*/
+type _Qmit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>
+/*将T中的所有属性变成必须的*/
+type _Required<T> = { [P in keyof T]-?: T[P] }
+
+type Pick_<T, K extends keyof T> = { [P in K]: T[P]; }
+type Partial_<T> = { [P in keyof T]?: T[P]; }
+type Record_<K extends keyof any, T> = { [P in K]: T }
+type Exclude_<T, U> = T extends U ? never : T;
+type Qmit_<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>
+type Required_<T> = { [P in keyof T]-?: T[P] }
